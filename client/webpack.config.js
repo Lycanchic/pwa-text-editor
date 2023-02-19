159d521b68a692b9f02486f709e432a3ba0d1885
Marie P <map40j@gmail.com>
2023-02-19 15:06:22 -0800

    updated the WebpackPwaManifest in the webpack.config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,13 +19,14 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: 'src/index.html',
         title: 'J.A.T.E'
       }),
       new InjectManifest ({
         swSrc: './src-sw.js',
-        swDest: 'src/sw.js',
+        swDest: 'dist/sw.js',
       }),
       new WebpackPwaManifest({
         name: 'Just Another Text Editor',
@@ -56,7 +58,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins:[ '@babel/pliugin-proposal-objec-rest-spread', '@babel/transform-runtime'],
+              plugins:[ '@babel/plugin-proposal-objec-rest-spread', '@babel/transform-runtime'],
             }
           }
         }
