30aa8d46fce0ef81d9f663d85422dcd6841570f5
Marie P <map40j@gmail.com>
2023-02-19 00:49:02 -0800

    updated databse.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const database = await openDB('jate', 1);
+    const tx = database.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, value: content });
+    await request;
+    console.log('Data successfully saved');
+  } catch (error) {
+    console.error('Error while putting data to the database', error);
+  }
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const database = await openDB('jate', 1);
+    const tx = database.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(1);
+    const result = await request; 
+    if (result) {
+      console.log('Data successfully retrieved', result.value);
+      return result.value;
+    } else {
+      console.log('Data not found');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error while getting data from the database', error);
+    return null;
+  }
+};
 
-initdb();
+initDb();
