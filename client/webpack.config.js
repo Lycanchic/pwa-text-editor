457a729e365bc7dbd87b8fdde7cd5f0182bbfa29
Marie P <map40j@gmail.com>
2023-02-19 15:51:50 -0800

    redoing the webpacl config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -19,52 +18,28 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        template: 'src/index.html',
+        template: './index.html',
         title: 'J.A.T.E'
       }),
-      new InjectManifest ({
-        swSrc: './src-sw.js',
-        swDest: 'src/sw.js',
-      }),
-      new WebpackPwaManifest({
-        name: 'Just Another Text Editor',
-        short_name: 'J.A.T.E',
-        description: 'JavaScript note taker',
-        background_color: '#ffffff',
-        theme_color: '#31a9e1',
-        start_url: '/',
-        public_path: '/',
-        icons: [
-          {
-            src: 'src/images/icon.png',
-            sizes: [96, 128, 192, 256, 384, 512],
-            type: 'image.png',
-          },
-        ],
-      }),
+      new InjectManifest({
+        
+
+      })
     ],
+
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: [ 'style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
+          test: /\.css$/,
           exclude: /node_modules/,
-          use:{
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins:[ '@babel/plugin-proposal-objec-rest-spread', '@babel/transform-runtime'],
+          use: [ 'style-loader', 'css-loader'
+            {
+              loader: 'ts-loader'
             }
-          }
+          ]
         }
-      ],
-    },
-   
+      ]
+    }
+   }
   };
- }
- console.log('Webpack build output: ', __dirname);
